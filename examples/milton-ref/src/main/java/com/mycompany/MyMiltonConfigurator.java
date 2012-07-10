e7ebf6d3f7d801665e2b5947a46a66673f0e23ab
Brad McEvoy <brad@mcevoysoftware.co.nz>
2012-07-10 21:37:06 +1200

    demonstrate LDAP

---
+++ examples/milton-ref/src/main/java/com/mycompany/MyMiltonConfigurator.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mycompany;
+
+import io.milton.ldap.LdapServer;
+import io.milton.ldap.LdapTransactionManager;
+import io.milton.ldap.NullLdapTransactionManager;
+import io.milton.servlet.DefaultMiltonConfigurator;
+
+/**
+ * Starts up additional services
+ *
+ * @author brad
+ */
+public class MyMiltonConfigurator extends DefaultMiltonConfigurator {
+
+    private TLdapUserFactory userFactory;
+    private LdapServer ldapServer;
+
+    @Override
+    protected void build() {
+        super.build();
+        userFactory = new TLdapUserFactory((TResourceFactory) configurer.getMainResourceFactory());
+        LdapTransactionManager transactionManager = new NullLdapTransactionManager();
+        ldapServer = new LdapServer(transactionManager, userFactory, configurer.getWebDavProtocol());
+        ldapServer.setPort(8369);
+        ldapServer.start();
+    }
+
+    @Override
+    public void shutdown() {
+        super.shutdown();
+    }
+}
