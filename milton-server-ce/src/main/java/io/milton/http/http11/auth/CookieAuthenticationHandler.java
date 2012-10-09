b2b6b0513f856941829071aaf910a3451257c0c0
Brad McEvoy <brad@mcevoysoftware.co.nz>
2012-10-09 22:12:55 +1300

    make attribute names configurable correctly handle logout

--- milton-server-ce/src/main/java/io/milton/http/http11/auth/CookieAuthenticationHandler.java
+++ milton-server-ce/src/main/java/io/milton/http/http11/auth/CookieAuthenticationHandler.java
@@ -30,6 +30,7 @@ public class CookieAuthenticationHandler implements AuthenticationHandler {
 	private String cookieUserUrlHash = "miltonUserUrlHash";
 	private final List<AuthenticationHandler> handlers;
 	private final ResourceFactory principalResourceFactory;
+	private String userUrlAttName = "userUrl";
 
 	public CookieAuthenticationHandler(List<AuthenticationHandler> handlers, ResourceFactory principalResourceFactory) {
 		this.handlers = handlers;
@@ -44,7 +45,6 @@ public class CookieAuthenticationHandler implements AuthenticationHandler {
 		// check for a logout command, if so logout
 		if (isLogout(request)) {
 			if (userUrl != null && userUrl.length() > 0) {
-				log.trace("logout: authId: " + userUrl);
 				clearCookieValue(HttpManager.response());
 			}
 		}
@@ -52,17 +52,14 @@ public class CookieAuthenticationHandler implements AuthenticationHandler {
 		for (AuthenticationHandler hnd : handlers) {
 			if (hnd.supports(r, request)) {
 				request.getAttributes().put(HANDLER_ATT_NAME, hnd);
-				log.debug("supports: true: " + hnd.getClass().getCanonicalName());
 				return true;
 			}
 		}
 
 		// We will support it if there is either a auth id request param
 		if (userUrl != null) {
-			log.debug("supports: found authId: " + userUrl);
 			return true;
 		} else {
-			log.debug("supports: false");
 			return false;
 		}
 	}
@@ -74,7 +71,6 @@ public class CookieAuthenticationHandler implements AuthenticationHandler {
 		// This would have been selected in the supports method
 		AuthenticationHandler delegateHandler = (AuthenticationHandler) request.getAttributes().get(HANDLER_ATT_NAME);
 		if (delegateHandler != null) {
-			log.trace("delegating to: " + delegateHandler);
 			// Attempt to authenticate against wrapped handler
 			// If successful generate a signed cookie and put into a request attribute
 			Object tag = delegateHandler.authenticate(resource, request);
@@ -94,28 +90,41 @@ public class CookieAuthenticationHandler implements AuthenticationHandler {
 		} else {
 			// No delegating handler means that we expect either to get a previous login token
 			// via a cookie, or this is an anonymous request
-			String userUrl = getUserUrl(request);
-			if (userUrl == null) {
-				// no token in request, so is anonymous
+			if (isLogout(request)) {
 				return null;
 			} else {
-				// we found a userUrl
-				String host = request.getHostHeader();
-				Resource r;
-				try {
-					r = principalResourceFactory.getResource(host, userUrl);
-				} catch (NotAuthorizedException ex) {
-					log.error("Couldnt check userUrl in cookie", ex);
-					r = null;
-				} catch (BadRequestException ex) {
-					log.error("Couldnt check userUrl in cookie", ex);
-					r = null;
+				String userUrl = getUserUrl(request);
+				if (userUrl == null) {
+					// no token in request, so is anonymous
+					return null;
+				} else {
+					// we found a userUrl
+					String host = request.getHostHeader();
+					Resource r;
+					try {
+						r = principalResourceFactory.getResource(host, userUrl);
+					} catch (NotAuthorizedException ex) {
+						log.error("Couldnt check userUrl in cookie", ex);
+						r = null;
+					} catch (BadRequestException ex) {
+						log.error("Couldnt check userUrl in cookie", ex);
+						r = null;
+					}
+					if (r == null) {
+						log.warn("User not found host: " + host + " userUrl: " + userUrl + " with resourcefactory: " + principalResourceFactory);
+						clearCookieValue(HttpManager.response());
+					} else {
+						// Logged in ok with details. Check if details came from request parameter, in
+						// which case we need to set cookies
+						if (r instanceof DiscretePrincipal) {
+							DiscretePrincipal dp = (DiscretePrincipal) r;
+							setLoginCookies(dp, request);
+						} else {
+							log.warn("Found user from request, but user object is not expected type. Should be " + DiscretePrincipal.class + " but is " + r.getClass());
+						}
+					}
+					return r;
 				}
-				if (r == null) {
-					log.warn("User not found host: " + host + " userUrl: " + userUrl + " with resourcefactory: " + principalResourceFactory);
-					clearCookieValue(HttpManager.response());
-				}
-				return r;
 			}
 		}
 	}
@@ -144,7 +153,7 @@ public class CookieAuthenticationHandler implements AuthenticationHandler {
 		String salt = Math.random() + "";
 		String signing = salt + ":" + DigestUtils.md5Hex(userUrl + ":" + salt);
 		setCookieValues(response, userUrl, signing);
-		request.getAttributes().put("userUrl", userUrl);
+		request.getAttributes().put(userUrlAttName, userUrl);
 	}
 
 	@Override
@@ -193,6 +202,7 @@ public class CookieAuthenticationHandler implements AuthenticationHandler {
 			userUrl = userUrl.trim();
 			if (userUrl.length() > 0) {
 				if (verifyHash(userUrl, request)) {
+
 					return userUrl;
 				} else {
 					log.error("Invalid userUrl hash, possible attempted hacking attempt");
@@ -241,6 +251,9 @@ public class CookieAuthenticationHandler implements AuthenticationHandler {
 	}
 
 	private String getCookieOrParam(Request request, String name) {
+		if (request == null) {
+			return null;
+		}
 		if (request.getParams() != null) {
 			String v = request.getParams().get(name);
 			if (v != null) {
@@ -261,4 +274,14 @@ public class CookieAuthenticationHandler implements AuthenticationHandler {
 	public String getCookieNameUserUrl() {
 		return cookieUserUrlValue;
 	}
+
+	public String getUserUrlAttName() {
+		return userUrlAttName;
+	}
+
+	public void setUserUrlAttName(String userUrlAttName) {
+		this.userUrlAttName = userUrlAttName;
+	}
+	
+	
 }
