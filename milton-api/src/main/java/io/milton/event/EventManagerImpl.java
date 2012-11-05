2d0136aee30c220bc5271c8aa067eedafb891ae0
Brad McEvoy <brad@mcevoysoftware.co.nz>
2012-11-06 12:15:26 +1300

    fix to allow registering listeners on event interfaces or subclasses

--- milton-api/src/main/java/io/milton/event/EventManagerImpl.java
+++ milton-api/src/main/java/io/milton/event/EventManagerImpl.java
@@ -24,6 +24,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,31 +35,38 @@ import org.slf4j.LoggerFactory;
 public class EventManagerImpl implements EventManager {
 
     private final static Logger log = LoggerFactory.getLogger( EventManagerImpl.class );
-    private final Map<Class, List<EventListener>> listenersMap = new HashMap<Class, List<EventListener>>();
+    private final List<Registration> registrations = new CopyOnWriteArrayList<Registration>();
 
     @Override
     public void fireEvent( Event e ) {
         if( log.isTraceEnabled() ) {
             log.trace( "fireEvent: " + e.getClass().getCanonicalName() );
         }
-        List<EventListener> list = listenersMap.get( e.getClass() );
-        if( list == null ) return;
-        for( EventListener l : Collections.unmodifiableCollection(list) ) {
-            if( log.isTraceEnabled() ) {
-                log.trace( "  firing on: " + l.getClass() );
+        for( Registration r : registrations) {
+            if( r.clazz.isAssignableFrom(e.getClass())) {
+                if( log.isTraceEnabled()) {
+                    log.trace( "  firing on: " + r.listener.getClass() );
+                }
+                r.listener.onEvent( e );
             }
-            l.onEvent( e );
         }
     }
 
     @Override
     public synchronized <T extends Event> void registerEventListener( EventListener l, Class<T> c ) {
         log.info( "registerEventListener: " + l.getClass().getCanonicalName() + " - " + c.getCanonicalName() );
-        List<EventListener> list = listenersMap.get( c );
-        if( list == null ) {
-            list = new ArrayList<EventListener>();
-            listenersMap.put( c, list );
+        Registration r = new Registration(l, c);
+        registrations.add(r);
+    }
+    
+    private class Registration {
+        private final EventListener listener;
+        private final Class<? extends Event> clazz;
+
+        public Registration(EventListener listener, Class<? extends Event> clazz) {
+            this.listener = listener;
+            this.clazz = clazz;
         }
-        list.add( l );
+                
     }
 }
