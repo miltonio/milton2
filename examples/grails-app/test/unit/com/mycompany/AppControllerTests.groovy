9b5d919a44524a1af11fcadfafed9c9df588dcb4
Brad McEvoy <brad@mcevoysoftware.co.nz>
2013-03-17 21:50:59 +1300

    example integration of grails and milton

---
+++ examples/grails-app/test/unit/com/mycompany/AppControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.mycompany
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(AppController)
+class AppControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
