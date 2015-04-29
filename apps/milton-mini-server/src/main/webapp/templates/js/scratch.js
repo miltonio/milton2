a6e11573c492c59842567504238efd782027fc53
brad <brad@brad-little-laptop>
2015-04-29 21:38:46 +1200

    2.6.5.3

--- apps/milton-mini-server/src/main/webapp/templates/js/scratch.js
+++ apps/milton-mini-server/src/main/webapp/templates/js/scratch.js
@@ -1,27 +1,27 @@
-{
-    "data":{
-        "data":[
-            {"date":1397649600000, "value":10}, 
-            {"date":1398254400000, "value":3}, 
-            {"date":1398340800000, "value":2}], 
-        "graphType":"Line", 
-        "itemFields":["item", "count"], 
-        "items":[
-            {"count":3, "item":"/websites/w1/version1/programs/ff/ff/ff1/"}, 
-            {"count":3, "item":"/programs/ff/ff/ff1/"}, 
-            {"count":2, "item":"/_postSearch"}, 
-            {"count":2, "item":"/websites/w1/version1/programs/p1/c1/"}, 
-            {"count":1, "item":"/websites/w1/version1/programs/p1/c1/m1/"}, 
-            {"count":1, "item":"/websites/w1/version1/programs/ff/ff/"}, 
-            {"count":1, "item":"/websites/"}, 
-            {"count":1, "item":"/websites/w1/version1/programs/ff/ff/ff1/index.html"}, 
-            {"count":1, "item":"/reporting/"}], 
-        "itemsTitle":"", 
-        "labels":["Hits"], 
-        "xkey":"date", 
-        "ykeys":["value"]}, 
-    "fieldMessages":[], 
-    "messages":[], 
-    "nextHref":"", 
-    "status":true
+function createCandidate(page, parameters, files) {
+    print("createCandidate " + parameters);
+    var json = '{"name": "xx"}';
+    print("save json " + json);
+    page.find("/jsondb/b2").createNew(parameters.get("newName"), json, "candidate");
+}
+
+function viewCandidate(page) {
+    print("viewCandidate " + page.name + " - " + page.class);
+    var candRes = page.find("/jsondb/b2").child(page.attributes.candId)
+    var ob = candRes.jsonObject;
+    print("cand name " + ob.name);
+}
+
+function searchCandidates(page, parameters) {
+    var jsonSearch = "{\n" +
+"\"fields\": [\"title\", \"category\",\"tags\"],\n" +
+"  \"query\" : {\n" +
+"    \"term\" : { \"content\" : \"farm\" }\n" +
+"}\n" +
+"}    \n" +
+"";
+
+    var results = page.find("/jsondb/b2").search(searchJson);
+    page.attributes.searchResults = results;
+    print("res " + page.attributes.searchResults);
 }
