4178fb7cfeb1beba9de80287d95bf1ef5ea56923
Wesley <tuzzmaniandevil@gmail.com>
2015-11-12 13:04:25 +1300

    Add attachment disposition to the StandardMessage interface

--- milton-mail-api/src/main/java/io/milton/mail/StandardMessageWrapper.java
+++ milton-mail-api/src/main/java/io/milton/mail/StandardMessageWrapper.java
@@ -23,11 +23,12 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Useful for wrapping a  standard message. You can then modify properties of
+ * Useful for wrapping a standard message. You can then modify properties of
  * this object which will override, but not affect, the wrapped object
  *
  */
-public class StandardMessageWrapper implements StandardMessage{
+public class StandardMessageWrapper implements StandardMessage {
+
     private final StandardMessage wrapped;
 
     private String subject;
@@ -43,161 +44,191 @@ public class StandardMessageWrapper implements StandardMessage{
         this.wrapped = wrapped;
     }
 
-    public void addAttachment(String name, String ct, String contentId, InputStream in) {
+    @Override
+    public void addAttachment(String name, String ct, String contentId, String disposition, InputStream in) {
         throw new UnsupportedOperationException("Not supported for wrapper.");
     }
 
+    @Override
     public List<StandardMessage> getAttachedMessages() {
         return wrapped.getAttachedMessages();
     }
 
+    @Override
     public void setAttachedMessages(List<StandardMessage> attachedMessages) {
         throw new UnsupportedOperationException("Not supported for wrapper.");
     }
 
+    @Override
     public String getSubject() {
-        if( subject != null ) {
+        if (subject != null) {
             return subject;
         } else {
             return wrapped.getSubject();
         }
     }
 
+    @Override
     public MailboxAddress getFrom() {
-        if( from != null ) {
+        if (from != null) {
             return from;
         } else {
             return wrapped.getFrom();
         }
     }
 
+    @Override
     public List<Attachment> getAttachments() {
         return wrapped.getAttachments();
     }
 
+    @Override
     public void setFrom(MailboxAddress from) {
         this.from = from;
     }
 
+    @Override
     public MailboxAddress getReplyTo() {
-        if( replyTo != null ) {
+        if (replyTo != null) {
             return wrapped.getReplyTo();
         } else {
             return wrapped.getReplyTo();
         }
     }
 
+    @Override
     public void setReplyTo(MailboxAddress replyTo) {
         this.replyTo = replyTo;
     }
 
+    @Override
     public void setSubject(String subject) {
         this.subject = subject;
     }
 
+    @Override
     public String getHtml() {
-        if( this.html != null ) {
+        if (this.html != null) {
             return html;
         } else {
             return wrapped.getHtml();
         }
     }
 
+    @Override
     public void setHtml(String html) {
         this.html = html;
     }
 
+    @Override
     public String getText() {
-        if( this.text != null ) {
+        if (this.text != null) {
             return text;
         } else {
             return wrapped.getText();
         }
     }
 
+    @Override
     public void setText(String text) {
         this.text = text;
     }
 
+    @Override
     public int getSize() {
         return wrapped.getSize();
     }
 
+    @Override
     public void setSize(int size) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public void setDisposition(String disposition) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public String getDisposition() {
         return wrapped.getDisposition();
     }
 
+    @Override
     public void setEncoding(String encoding) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public String getEncoding() {
         return wrapped.getEncoding();
     }
 
+    @Override
     public void setContentLanguage(String contentLanguage) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public String getContentLanguage() {
         return wrapped.getContentLanguage();
     }
 
+    @Override
     public Map<String, String> getHeaders() {
         return wrapped.getHeaders();
     }
 
+    @Override
     public void setHeaders(Map<String, String> headers) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public List<MailboxAddress> getTo() {
-        if( to != null ) {
+        if (to != null) {
             return to;
         } else {
             return wrapped.getTo();
         }
     }
 
+    @Override
     public void setTo(List<MailboxAddress> to) {
         this.to = to;
     }
 
+    @Override
     public List<MailboxAddress> getCc() {
-        if( cc != null ) {
+        if (cc != null) {
             return cc;
         } else {
             return wrapped.getCc();
         }
     }
 
+    @Override
     public void setCc(List<MailboxAddress> cc) {
         this.cc = cc;
     }
 
+    @Override
     public List<MailboxAddress> getBcc() {
-        if( this.bcc != null ) {
+        if (this.bcc != null) {
             return this.bcc;
         } else {
             return wrapped.getBcc();
         }
     }
 
+    @Override
     public void setBcc(List<MailboxAddress> bcc) {
         this.bcc = bcc;
     }
 
+    @Override
     public StandardMessage instantiateAttachedMessage() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
 
 }
