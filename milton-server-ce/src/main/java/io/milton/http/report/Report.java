4137a598c76e0aa41d1033982d81abed22f49b7b
brad <brad@kademi.co>
2018-03-02 13:21:48 +1300

    2.7.4.2

--- milton-server-ce/src/main/java/io/milton/http/report/Report.java
+++ milton-server-ce/src/main/java/io/milton/http/report/Report.java
@@ -32,21 +32,21 @@ import io.milton.http.exceptions.NotAuthorizedException;
 public interface Report {
     /**
      * The name of the report, as used in REPORT requests
-     * 
+     *
      * @return
      */
     String getName();
 
-	
+
     /**
      * Process the requested report body, and return a document containing the
      * response body.
      *
      * Must be a multistatus response.
      *
-     * @param host 
-	 * @param path 
-     * @param r 
+     * @param host
+	 * @param path
+     * @param r
      * @param doc
      * @return the response body, usually xml
      */
